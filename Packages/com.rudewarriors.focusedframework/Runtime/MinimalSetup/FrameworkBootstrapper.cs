9e03e113f81eb0cd976b0e32df8376d0639f503f
Reaper-RudeWarriorsStudio <reaperss.inc@gmail.com>
2025-10-13 12:50:08 -0500

    Create FrameworkBootstrapper.cs

---
+++ Packages/com.rudewarriors.focusedframework/Runtime/MinimalSetup/FrameworkBootstrapper.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using RudeWarriors.Framework.Core;
+
+public class FrameworkBootstrapper : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        // Register core services once.
+        ServiceLocator.Register<IEventBus>(new EventBus());
+        ServiceLocator.Register<IDataService>(new DataService());
+
+        // Add your own: ServiceLocator.Register<ISomeService>(new SomeService());
+    }
+}
